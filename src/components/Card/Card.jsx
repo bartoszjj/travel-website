d1e771d401ddea27fa1a69cd89ece3a0503beea7
Bartosz <101667142+bartoszjj@users.noreply.github.com>
2025-01-10 14:11:22 +0100

    added labels to the cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
 
-function Card({ src, alt, title }) {
+function Card({ src, alt, title, label }) {
   return (
     <li className={styles.card}>
       <Link>
         <div className={styles.imageWrapper}>
           <img src={src} alt={alt} />
+          <span className={styles.label}>{label}</span>
         </div>
-
         <h3 className={styles.title}>{title}</h3>
       </Link>
     </li>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,26 +11,31 @@ function Cards() {
           "A man standing atop a rock in front of a waterfall in the Amazon Jungle"
         }
         title={"Explore the hidden waterfall deep inside the Amazon Jungle"}
+        label={"Adventure"}
       ></Card>
       <Card
         src={"/images/img-2.jpg"}
         alt={"Aerial view of the Islands of Bali"}
         title={"Travel through the Islands of Bali in a Private Cruise"}
+        label={"Luxury"}
       ></Card>
       <Card
         src={"/images/img-3.jpg"}
         alt={"Aerial view of a boat in the ocean"}
         title={"Set Sail in the Atlantic Ocean visiting Uncharted Waters"}
+        label={"Mystery"}
       ></Card>
       <Card
         src={"/images/img-4.jpg"}
         alt={"Aerial view of a football field atop a mountain"}
         title={"Experience Football on Top of the Himalayan Mountains"}
+        label={"Adventure"}
       ></Card>
       <Card
         src={"/images/img-8.jpg"}
         alt={"A line of camels wandering among sand dunes"}
         title={"Ride through the Sahara Desert on a guided camel tour"}
+        label={"Adrenaline"}
       ></Card>
     </ul>
   );
