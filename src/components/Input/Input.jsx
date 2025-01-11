aaa9121d9c89f828fbb663b60805512efbf8ff57
Bartosz <101667142+bartoszjj@users.noreply.github.com>
2025-01-11 17:18:14 +0100

    Sign Up form almost finished

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import styles from "./Input.module.css";
-function Input({ ...delegated }) {
-  return <input className={styles.inputStyles} {...delegated}></input>;
+function Input({ id, label, border, inputSize, ...delegated }) {
+  const generatedId = React.useId();
+  const appliedId = id || generatedId;
+
+  return (
+    <div className={styles.inputWrapper}>
+      {label && (
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <input
+        id={appliedId}
+        className={`${styles.inputStyles} ${
+          border ? styles.border : undefined
+        } ${inputSize}`}
+        {...delegated}
+      ></input>
+    </div>
+  );
 }
 
 export default Input;
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Input from "../Input/Input";
+import styles from "./SignUp.module.css";
+import Button from "../Button/Button";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className={styles.signUpWrapper}>
+      <h2>Sign Up</h2>
+      <form>
+        <Input
+          inputSize={styles.inputSize}
+          label="E-mail"
+          border={true}
+          type="email"
+          placeholder="Enter your e-mail"
+        ></Input>
+        <Input
+          inputSize={styles.inputSize}
+          label="Username"
+          border={true}
+          type="text"
+          placeholder="Enter your username"
+        ></Input>
+        <Input
+          inputSize={styles.inputSize}
+          label="Password"
+          border={true}
+          type="password"
+          placeholder="Enter your password"
+        ></Input>
+        <Button>Sign Up</Button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
