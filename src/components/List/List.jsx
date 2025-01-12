ec7311702570f221502e109d9cfb26cf678e5fa4
Bartosz <101667142+bartoszjj@users.noreply.github.com>
2025-01-12 21:52:54 +0100

    added the work in progress modal to links

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./List.module.css";
 import { Link } from "react-router-dom";
+import Modal from "../Modal/Modal";
 
 function List({ heading, list }) {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
@@ -12,10 +13,25 @@ function List({ heading, list }) {
         {list.map(({ listItem, url }, key) => {
           return (
             <li key={key}>
-              <Link to={url}>{listItem}</Link>
+              <Link
+                onClick={(event) => {
+                  if (url === undefined) {
+                    event.preventDefault();
+                    setIsModalOpen(true);
+                  }
+                }}
+                to={url}
+              >
+                {listItem}
+              </Link>
             </li>
           );
         })}
+        {isModalOpen && (
+          <Modal setIsModalOpen={setIsModalOpen}>
+            <p>Work in progress</p>
+          </Modal>
+        )}
       </ul>
     </div>
   );
