b5b188bd245a071c6dab853d887eab22ff5d0397
Bartosz <101667142+bartoszjj@users.noreply.github.com>
2025-01-15 17:47:04 +0100

    Finished validation

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Input from "../Input/Input";
 import styles from "./SignUp.module.css";
 import Button from "../Button/Button";
+import { CircleAlert } from "lucide-react";
 
 function SignUp({ closeModal }) {
   const [email, setEmail] = React.useState("");
@@ -12,71 +13,64 @@ function SignUp({ closeModal }) {
   const [usernameErrors, setUsernameErrors] = React.useState([]);
   const [passwordErrors, setPasswordErrors] = React.useState([]);
 
-  // function validateEmail() {
-  //   const tempPasswordErrors = [];
-  //   // if (password.length < 10) {
-  //   //   const error = "Password has to be more than 10 characters long";
-  //   //   tempPasswordErrors.push(error);
-  //   //   setPasswordErrors(tempPasswordErrors);
-  //   // }
-  //   // if (password.length == 0) {
-  //   //   const error = "You have not entered any password";
-  //   //   tempPasswordErrors.push(error);
-  //   //   setPasswordErrors(tempPasswordErrors);
-  //   // }
-  //   return tempPasswordErrors.length;
-  // }
+  function validateEmail() {
+    const tempEmailErrors = [];
+    if (email.length == 0) {
+      const error = "You have not entered any E-mail";
+      tempEmailErrors.push(error);
+    }
 
-  // function validateUsername() {
-  //   const tempPasswordErrors = [];
-  //   // if (password.length < 10) {
-  //   //   const error = "Password has to be more than 10 characters long";
-  //   //   tempPasswordErrors.push(error);
-  //   //   setPasswordErrors(tempPasswordErrors);
-  //   // }
-  //   // if (password.length == 0) {
-  //   //   const error = "You have not entered any password";
-  //   //   tempPasswordErrors.push(error);
-  //   //   setPasswordErrors(tempPasswordErrors);
-  //   // }
-  //   return tempPasswordErrors.length;
-  // }
+    if (!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+      const error = "E-mail is invalid";
+      tempEmailErrors.push(error);
+    }
+    setEmailErrors(tempEmailErrors);
+    return tempEmailErrors.length;
+  }
+
+  function validateUsername() {
+    const tempUsernameErrors = [];
+    if (username.length < 3 || username.length > 30) {
+      const error = "Username must be between 3 and 30 characters long";
+      tempUsernameErrors.push(error);
+    }
+    if (!username.match(/^[a-zA-Z0-9_-]+$/)) {
+      const error = "Username is invalid";
+      tempUsernameErrors.push(error);
+    }
+
+    setUsernameErrors(tempUsernameErrors);
+    return tempUsernameErrors.length;
+  }
 
   function validatePassword() {
     const tempPasswordErrors = [];
+    if (password.length == 0) {
+      const error = "You have not entered any password";
+      tempPasswordErrors.push(error);
+    }
     if (password.length < 10) {
       const error = "Password has to be more than 10 characters long";
       tempPasswordErrors.push(error);
     }
-    if (password.length == 0) {
-      console.log("You have not entered any password");
-      const error = "You have not entered any password";
-      tempPasswordErrors.push(error);
-    }
-    if (!password.match(/[^a-zA-Z]/)) {
-      console.log("Password must contain at least one special character");
-      const error = "Password must contain at least one special character";
-      tempPasswordErrors.push(error);
-      setPasswordErrors(tempPasswordErrors);
-    }
-
     if (!password.match(/[A-Z]/)) {
-      console.log("Password must contain at least one uppercase letter");
       const error = "Password must contain at least one uppercase letter";
       tempPasswordErrors.push(error);
     }
-
-    console.log(tempPasswordErrors.length);
+    if (!password.match(/[^a-zA-Z1-9]/)) {
+      const error = "Password must contain at least one special character";
+      tempPasswordErrors.push(error);
+    }
     setPasswordErrors(tempPasswordErrors);
     return tempPasswordErrors.length;
   }
 
   function signUp(event) {
     event.preventDefault();
-    // if (validateEmail() && validateUsername() && validatePassword()) {
-    //   return;
-    // }
-    if (validatePassword()) {
+    const emailErrorsLength = validateEmail();
+    const usernameErrorsLength = validateUsername();
+    const passwordErrorsLength = validatePassword();
+    if (emailErrorsLength + usernameErrorsLength + passwordErrorsLength !== 0) {
       return;
     }
     closeModal();
@@ -88,9 +82,10 @@ function SignUp({ closeModal }) {
       <form onSubmit={signUp}>
         <Input
           inputSize={styles.inputSize}
+          errorStyles={emailErrors.length > 0 && styles.errorStyles}
           label="E-mail"
           border={true}
-          type="email"
+          type="text"
           placeholder="Enter your e-mail"
           // required={true}
           value={email}
@@ -98,8 +93,20 @@ function SignUp({ closeModal }) {
             setEmail(e.target.value);
           }}
         ></Input>
+        {emailErrors.length > 0 &&
+          emailErrors.map((error, key) => {
+            return (
+              <p key={key} className={styles.errorMessage}>
+                <span>
+                  <CircleAlert size={18} />
+                </span>
+                <span>{error}</span>
+              </p>
+            );
+          })}
         <Input
           inputSize={styles.inputSize}
+          errorStyles={usernameErrors.length > 0 && styles.errorStyles}
           label="Username"
           border={true}
           type="text"
@@ -110,6 +117,17 @@ function SignUp({ closeModal }) {
             setUsername(e.target.value);
           }}
         ></Input>
+        {usernameErrors.length > 0 &&
+          usernameErrors.map((error, key) => {
+            return (
+              <p key={key} className={styles.errorMessage}>
+                <span>
+                  <CircleAlert size={18} />
+                </span>
+                <span>{error}</span>
+              </p>
+            );
+          })}
         <Input
           inputSize={styles.inputSize}
           errorStyles={passwordErrors.length > 0 && styles.errorStyles}
@@ -127,7 +145,9 @@ function SignUp({ closeModal }) {
           passwordErrors.map((error, key) => {
             return (
               <p key={key} className={styles.errorMessage}>
-                <span>•</span>
+                <span>
+                  <CircleAlert size={18} />
+                </span>
                 <span>{error}</span>
               </p>
             );
